035bdc1fc1d35221d4269752dab19c468c66beaf
MRLunch <patwzc@gmail.com>
2024-07-26 23:04:27 +1200

    modify main.js

--- worksheet2/my-nest-app/src/main.ts
+++ worksheet2/my-nest-app/src/main.ts
@@ -5,6 +5,7 @@ import * as dotenv from 'dotenv';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   dotenv.config();
-  await app.listen(3000);
+  const PORT = process.env.PORT || 5000;
+  await app.listen(PORT);
 }
 bootstrap();
