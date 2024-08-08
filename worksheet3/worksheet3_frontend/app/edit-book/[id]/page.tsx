ef0f05b706c2874b7035b04f591a3af67bf65146
MRLunch <patwzc@gmail.com>
2024-08-08 16:59:29 +1200

    create book components

---
+++ worksheet3/worksheet3_frontend/app/edit-book/[id]/page.tsx
@@ -0,0 +1,12 @@
+// enable client-side rendering for this file
+"use client";
+
+// import UpdateBookInfo component from the components folder
+import UpdateBookInfo from "@src/components/UpdateBookInfo";
+
+// define and export the default function component ShowBook
+export default function ShowBook() {
+  // Return JSX for the ShowBook component
+  return <UpdateBookInfo />;
+}
+// defines a React functional component 'Showbook', imports the 'UpdateBookInfo' component and renders it, typically used for dynamic route where 'UpdateBookInfo' component will handle the logic for updating a book's information based on its ID.
