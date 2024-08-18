d05263d7b95282135d9bec9dffdc641d52823c0d
MRLunch <patwzc@gmail.com>
2024-08-18 21:43:54 +1200

    update main.ts

--- worksheet4/backend/worksheet4_backend/dist/main.js
+++ worksheet4/backend/worksheet4_backend/dist/main.js
@@ -5,7 +5,7 @@ const app_module_1 = require("./app.module");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.enableCors({
-        origin: 'http://localhost:3000',
+        origin: process.env.FRONTEND_URL || 'http://localhost:3000',
         credentials: true
     });
     const port = process.env.PORT || 8082;
--- worksheet4/backend/worksheet4_backend/src/main.ts
+++ worksheet4/backend/worksheet4_backend/src/main.ts
@@ -3,12 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  //enable cors
   app.enableCors({
-    origin: 'http://localhost:3000',//frontend locahost
+    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
     credentials: true
   });
   const port = process.env.PORT || 8082;
   await app.listen(port, () => console.log(`Server running on port ${port}`));
 }
+
 bootstrap();
