c821d2b07d1503bad2acf818ec00cc5c4ff20f8d
MRLunch <patwzc@gmail.com>
2024-08-18 20:49:45 +1200

    Update main.ts

--- worksheet4/backend/worksheet4_backend/src/main.ts
+++ worksheet4/backend/worksheet4_backend/src/main.ts
@@ -2,9 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // Enable CORS with dynamic origin
+  //enable cors
   app.enableCors({
-    origin: process.env.CORS_ORIGIN || 'http://localhost:3000',
+    origin: 'http://localhost:3000',//frontend locahost
     credentials: true
   });
   const port = process.env.PORT || 8082;
